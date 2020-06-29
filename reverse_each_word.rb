f33c27a5b3d962d809f6c602000c8e9035154899
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-06-29 23:05:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,4 @@
 def reverse_each_word(string)
   split_string = string.split(" ")
   reversed = []
-  string.size.times do
-    reversed << split_string.pop
-  end
-  reversed.join
-end
+  reversed = 
